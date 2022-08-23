57e220f2f4e8c51874809e35ee7f89498c125ea1
Napon Saisaoad <napon@odds.team>
2022-08-23 22:48:20 +0700

    Add input and test input

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -3,7 +3,18 @@ import {Component} from "react";
 class SignUpPage extends Component {
     render() {
         return (
+            <div> 
             <h1>Sign Up</h1>
+            <label htmlFor="username"> Username </label>
+            <input id="username"/>
+            <label  htmlFor="email"> Email </label>
+            <input id="email"/>
+            <label  htmlFor="password"> Password </label>
+            <input type="password" id="password"/>
+            <label  htmlFor="passwordRepeat"> Password Repeat</label>
+            <input type="password" id="passwordRepeat"/>
+            <button disabled>Sign Up</button>
+            </div> 
         )
     }
 }

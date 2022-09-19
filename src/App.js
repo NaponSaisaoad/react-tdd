1fe88598761daa69c34637d77869c952a94191bf
Napon Saisaoad <napon@odds.team>
2022-09-20 00:57:23 +0700

    Add home page and add nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import SignUpPage from "./pages/SignUpPage";
+import SignUpPage from './pages/SignUpPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import UserPage from './pages/UserPage';
+import AccountActivationPage from './pages/AccountActivationPage';
+import LanguageSelector from './components/LanguageSelector';
+import { Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="container">
-   <SignUpPage/>
+    <>
+    <NavBar />
+    <div className="container pt-3">
+      <Route exact path="/" component={HomePage} />
+      <Route path="/signup" component={SignUpPage} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/user/:id" component={UserPage} />
+      <Route path="/activate/:token" component={AccountActivationPage} />
+      <LanguageSelector />
     </div>
+  </>
   );
 }
 
---
+++ src/pages/Homepage.js
@@ -0,0 +1,35 @@
+import App from './App';
+import { render, screen } from './test/setup';
+
+const setup = (path) => {
+    window.history.pushState({}, '', path);
+    render(<App />);
+  };
+
+describe('Routing', () => {
+    it.each`
+    path               | pageTestId
+    ${'/'}             | ${'home-page'}
+    ${'/signup'}       | ${'signup-page'}
+    ${'/login'}        | ${'login-page'}
+  `('displays $pageTestId when path is $path', ({ path, pageTestId }) => {
+    setup(path);
+    const page = screen.queryByTestId(pageTestId);
+    expect(page).toBeInTheDocument();
+  });
+
+  it.each`
+    path               | pageTestId
+    ${'/'}             | ${'signup-page'}
+    ${'/'}             | ${'login-page'}
+    ${'/signup'}       | ${'home-page'}
+    ${'/signup'}       | ${'login-page'}
+  `(
+    'does not display $pageTestId when path is $path',
+    ({ path, pageTestId }) => {
+      setup(path);
+      const page = screen.queryByTestId(pageTestId);
+      expect(page).not.toBeInTheDocument();
+    }
+  );
+});

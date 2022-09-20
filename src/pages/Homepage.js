5a33955d4716abd8ff1de19001d1264fccbce499
Napon Saisaoad <napon@odds.team>
2022-09-20 21:34:38 +0700

    Add user list and edit home page file

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,35 +1,10 @@
-import App from './App';
-import { render, screen } from './test/setup';
-
-const setup = (path) => {
-    window.history.pushState({}, '', path);
-    render(<App />);
-  };
-
-describe('Routing', () => {
-    it.each`
-    path               | pageTestId
-    ${'/'}             | ${'home-page'}
-    ${'/signup'}       | ${'signup-page'}
-    ${'/login'}        | ${'login-page'}
-  `('displays $pageTestId when path is $path', ({ path, pageTestId }) => {
-    setup(path);
-    const page = screen.queryByTestId(pageTestId);
-    expect(page).toBeInTheDocument();
-  });
-
-  it.each`
-    path               | pageTestId
-    ${'/'}             | ${'signup-page'}
-    ${'/'}             | ${'login-page'}
-    ${'/signup'}       | ${'home-page'}
-    ${'/signup'}       | ${'login-page'}
-  `(
-    'does not display $pageTestId when path is $path',
-    ({ path, pageTestId }) => {
-      setup(path);
-      const page = screen.queryByTestId(pageTestId);
-      expect(page).not.toBeInTheDocument();
-    }
+import UserList from '../components/UserList';
+const HomePage = () => {
+  return (
+    <div data-testid="home-page">
+      <UserList />
+    </div>
   );
-});
+};
+
+export default HomePage;

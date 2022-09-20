d0807950fcab6c48223eae8c14cfc181170399cf
Napon Saisaoad <napon@odds.team>
2022-09-20 23:39:22 +0700

    Add avigates to user page when clicking the username on user list

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,10 +1,36 @@
 import App from './App';
 import { render, screen } from './test/setup';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
 
 const setup = (path) => {
     window.history.pushState({}, '', path);
     render(<App />);
   };
+
+  const server = setupServer(
+    rest.post('/api/1.0/users/token/:token', (req, res, ctx) => {
+      return res(ctx.status(200));
+    }),
+    rest.get('/api/1.0/users', (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          content: [
+            {
+              id: 1,
+              username: 'user-in-list',
+              email: 'user-in-list@mail.com',
+              image: null
+            }
+          ],
+          page: 0,
+          size: 0,
+          totalPages: 0
+        })
+      );
+    }),
+  )
 
 describe('Routing', () => {
     it.each`

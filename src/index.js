27eb325669957153ff02f5ea992dc5a04ab29af4
Napon Saisaoad <napon@odds.team>
2022-09-21 23:43:16 +0700

    Edit locale

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./locale/i18n";
+import './locale/i18n';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import createStore from './state/store';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Provider store={createStore()}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/pages/components/Spinner.js
+++ src/pages/components/Spinner.js
@@ -1,8 +1,8 @@
 const Spinner = (props) => {
-    let spanClass = 'spinner-border';
-    if (props.size !== 'big') {
-      spanClass += ' spinner-border-sm';
-    }
-    return <span className={spanClass} role="status"></span>;
-  };
-  export default Spinner;
+  let spanClass = 'spinner-border';
+  if (props.size !== 'big') {
+    spanClass += ' spinner-border-sm';
+  }
+  return <span className={spanClass} role="status"></span>;
+};
+export default Spinner;

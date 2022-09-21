09269656c71a879e9ae8be8d926951686033b28f
Napon Saisaoad <napon@odds.team>
2022-09-21 23:37:07 +0700

    Add modal and input

--- src/pages/components/input.spec.js
+++ src/pages/components/input.spec.js
@@ -1,21 +1,20 @@
-import { render } from "@testing-library/react";
-import Input from "./input";
+import { render } from '../test/setup';
+import Input from './Input';
 
-it("has is-invalid class for input when help is set", () => {
-   const { container } = render(<Input help="Error message"/>);
-   const input = container.querySelector("input");
-   expect(input.classList).toContain('is-inValid')
-})
+it('has is-invalid class for input when help is set', () => {
+  const { container } = render(<Input help="Error message" />);
+  const input = container.querySelector('input');
+  expect(input.classList).toContain('is-invalid');
+});
 
-it("has is-feedback class for input when help is set", () => {
-    const { container } = render(<Input help="Error message"/>);
-    const span = container.querySelector("span");
-    expect(span.classList).toContain('is-inValid')
-})
+it('has invalid-feedback class for span when help is set', () => {
+  const { container } = render(<Input help="Error message" />);
+  const span = container.querySelector('span');
+  expect(span.classList).toContain('invalid-feedback');
+});
 
-it("does not has is-invalid class for input when help is not set", () => {
-    const { container } = render(<Input />);
-    const input = container.querySelector("input");
-    expect(input.classList).toContain('is-inValid')
-})
-
+it('does not have is-invalid class for input when help is not set', () => {
+  const { container } = render(<Input />);
+  const input = container.querySelector('input');
+  expect(input.classList).not.toContain('is-invalid');
+});
